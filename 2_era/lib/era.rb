9a9e618dff9177ac65d0661f15214f59a6ed6689
Tanidaiz <tanidaig@yahoo.co.jp>
2021-11-07 14:05:41 +0900

    add: test_era1

---
+++ 2_era/lib/era.rb
@@ -0,0 +1,12 @@
+
+class Era
+    # 元号変換ロジック
+    #
+    # @param year [Integer] 西暦
+    # @param month [Integer] 月(1〜12)
+    # @param day [Integer] 日(1〜31)
+    # @return [String] 元号変換後の文字列 ex) "平成10年"
+    def calc(year, month, day)
+        '平成11年'
+    end
+end
